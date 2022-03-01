42d0347b0fa3d5096bcfb4b234ae4b9095925a8b
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-03-01 04:44:46 +0100

    Finita classe Room con metodi

--- classes/Room.php
+++ classes/Room.php
@@ -1,11 +1,27 @@
 <?php
-class Room {
+class Room
+{
     protected $number;
     protected $seats;
-    
+
     public function __construct($_number, $_seats = 150)
     {
         $this->number = $_number;
         $this->seats = $_seats;
+    }
+
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    public function setNumber($_number)
+    {
+        $this->number = $_number;
+    }
+
+    public function sellTicket($seat, $name)
+    {
+        $this->seats[$seat] = $name;
     }
 }
