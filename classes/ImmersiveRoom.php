f521ad6d725252a5cbe538aa4b3cc67e5f1369b1
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-03-01 05:01:56 +0100

    cambiata indentazione immersiveRoom.php

--- classes/ImmersiveRoom.php
+++ classes/ImmersiveRoom.php
@@ -1,22 +1,27 @@
 <?php
 
-class ImmersiveRoom extends Room {
+class ImmersiveRoom extends Room
+{
     private $specialEffects;
 
-    public function __construct($_number, $_seats = 300, $_specialEffects) {
+    public function __construct($_number, $_seats = 300, $_specialEffects)
+    {
         parent::__construct($_number, $_seats = 300);
         $this->specialEffects = $_specialEffects;
     }
 
-    public function addSpecialEffect($effect) {
+    public function addSpecialEffect($effect)
+    {
         $this->specialEffects[] = $effect;
     }
 
-    public function getSpecialEffects() {
+    public function getSpecialEffects()
+    {
         return $this->specialEffects;
     }
 
-    public function getSpecialEffectsString() {
+    public function getSpecialEffectsString()
+    {
         $output = "";
         foreach ($this->specialEffects as $effect) {
             $output .= $effect . " ";
