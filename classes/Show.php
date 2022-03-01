2da7d884491e7ccb5ca7ab27c653e92509189304
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-03-01 05:11:00 +0100

    Aggiunti metodi a Show.php

--- classes/Show.php
+++ classes/Show.php
@@ -1,5 +1,6 @@
 <?php
-class Show {
+class Show
+{
     private $movie;
     private $room;
     private $time;
@@ -12,8 +13,52 @@ class Show {
         $this->room = $_room;
         $this->time = $_time;
         $this->priceFull = $_priceFull;
-        $this->priceReduced = $_priceFull - ($_priceFull*50)/100;
+        $this->priceReduced = $_priceFull - ($_priceFull * 50) / 100;
     }
 
-    
+    public function getMovie()
+    {
+        return $this->movie;
+    }
+
+    public function setMovie($_movie)
+    {
+        $this->movie = $_movie;
+    }
+
+    public function getRoom()
+    {
+        return $this->room;
+    }
+
+    public function setRoom($_room)
+    {
+        $this->room = $_room;
+    }
+
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+    public function setTime($_time)
+    {
+        $this->time = $_time;
+    }
+
+    public function getPriceFull()
+    {
+        return $this->priceFull;
+    }
+
+    public function getPriceReduced()
+    {
+        return $this->priceReduced;
+    }
+
+    public function setPrice($_priceFull)
+    {
+        $this->priceFulll = $_priceFull;
+        $this->priceReduced = $_priceFull - ($_priceFull * 50) / 100;
+    }
 }
