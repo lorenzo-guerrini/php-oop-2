524944dae79d09b8cd9ccd5e0171dabb1c0f5b6b
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-03-01 04:30:09 +0100

    Copiato file Movie.php

---
+++ classes/Movie.php
@@ -0,0 +1,47 @@
+<?php
+class Movie
+{
+    private $title;
+    private $genres;
+
+    public function __construct($_title, $_genres)
+    {
+        $this->title = $_title;
+        $this->genres = $_genres;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($_title)
+    {
+        $this->title = $_title;
+    }
+
+    public function getGenres()
+    {
+        return $this->genres;
+    }
+
+    public function addGenresArray($_genre)
+    {
+        array_push($this->genres, $_genre);
+    }
+
+    public function getGenresString()
+    {
+        $output = "";
+        foreach ($this->genres as $genre) {
+            $output .= $genre . " ";
+        }
+        return $output;
+    }
+
+    public function getInfo()
+    {
+        $genres = $this->getGenresString();
+        return "{$this->title} {$genres}";
+    }
+}
